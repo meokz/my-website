e81764f4626bf19a698cd7d87cc57d09190afe48
Kazuki Otao <kaootao@gmail.com>
2019-02-24 15:19:52 +0900

    プロフィールのリダイレクト対応

---
+++ pages/profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      titleTemplate: null,
+      title: '大峠和基 / Kazuki Otao',
+    }
+  },
+  layout: 'base',
+  beforeCreate() {
+    this.$router.push('/')
+  }
+}
+</script>
