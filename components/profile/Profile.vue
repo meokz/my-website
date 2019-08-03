a0f5d296917249c7f0ad679656714632db9937fb
Kazuki Otao <kaootao@gmail.com>
2019-08-03 14:42:38 +0900

    修正

--- components/profile/Profile.vue
+++ components/profile/Profile.vue
@@ -28,7 +28,7 @@
 import Carousel from '~/components/profile/Carousel.vue'
 
 const profile = {
-  jp: "1996年広島生まれ．筑波大学大学院図書館情報メディア研究科．ピクシーダストテクノロジーズ株式会社エンジニア．ソフトウェア開発が得意で，U.22プログラミング・コンテスト（経済産業省商務情報政策局長賞 / 企業賞），全国高専プログラミングコンテスト（敢闘賞2回）に出場．2017年より落合陽一率いるデジタルネイチャー研究室に所属し，空中映像およびバーチャルリアリティを専門に研究を行う（書籍1本，学会発表10件）．筑波大学ミスコン2018でミスターキャンパス賞を受賞．2019年度「未踏事業」採択．筑波大学（学士，茗渓会賞），徳山高専（準学士，文化功労賞）を卒業．",
+  jp: "1996年広島生まれ．筑波大学大学院図書館情報メディア研究科．ピクシーダストテクノロジーズ株式会社エンジニア．ソフトウェア開発が得意で，U.22プログラミング・コンテスト（経済産業省商務情報政策局長賞 / 企業賞），全国高専プログラミングコンテスト（敢闘賞2回）に出場．2017年より落合陽一率いるデジタルネイチャー研究室に所属し，空中映像およびバーチャルリアリティを専門に研究を行う（書籍1本，学会発表11件）．筑波大学ミスコン2018でミスターキャンパス賞を受賞．2019年度「未踏事業」採択．筑波大学（学士，茗渓会賞），徳山高専（準学士，文化功労賞）を卒業．",
   en: "Kazuki Otao was born in 1996. After studying computer science and electronic engineering at National Institute of Technology, Tokuyama College since 2012, he is incorporated into the College of Media Arts, Science and Technology at the University of Tsukuba in 2017. He got a prize in many contests including U-22 programming contest and national college of technology programming contest. He is an expert researcher of the aerial imaging system in Digital Nature Group."
 }
 
