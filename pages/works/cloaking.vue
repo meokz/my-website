362295d6673f93cc5667748ba19accf110e07152
Kazuki Otao <kaootao@gmail.com>
2019-10-10 15:19:04 +0900

    PDFの大きさを書いた

--- pages/works/cloaking.vue
+++ pages/works/cloaking.vue
@@ -7,7 +7,7 @@
         <h3><span class="glyphicon glyphicon-link"></span>Web</h3>
         <p><a href="https://digitalnature.slis.tsukuba.ac.jp/2019/08/optical-cloaking-display/" target="_blank">Optical Cloaking Display - Digital Nature Group</a></p>
         <h3><span class="glyphicon glyphicon-file"></span>Documents</h3>
-        <p><a href="/works/cloaking/docs/cloaking_siggraph_2019_etech.pdf" target="_blank">SIGGRAPH 2019 Emerging Technologies Two-page Abstract [PDF X.XMB]</a></p>
+        <p><a href="/works/cloaking/docs/cloaking_siggraph_2019_etech.pdf" target="_blank">SIGGRAPH 2019 Emerging Technologies Two-page Abstract [PDF 1.87MB]</a></p>
         <h3><span class="glyphicon glyphicon-list"></span>Publication</h3>
         <p><a href="https://doi.org/10.1145/3305367.3327979" target="_blank">Takahito Aoto, Yuta Itoh, <u>Kazuki Otao</u>, Kazuki Takazawa, and Yoichi Ochiai. 2018. A design for optical cloaking display. In <em>SIGGRAPH ’19 Emerging Technologies</em> (SIGGRAPH ’19). ACM, New York, NY, USA, 2 pages. DOI: https://doi.org/10.1145/3305367.3327979</a></p>
         <h3><span class="glyphicon glyphicon-bookmark"></span>Achievement</h3>
