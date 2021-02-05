c74d800ee4bd4342616d1040eb58af5941e2f183
Kazuki Otao <kaootao@gmail.com>
2021-02-05 15:23:05 +0900

    インターン情報の更新

--- components/profile/Career.vue
+++ components/profile/Career.vue
@@ -10,6 +10,10 @@
       <ul>
         <li v-for="item in carrer.work_jp" v-bind:key="item">{{ item }}</li>
       </ul>
+      <h4>インターン</h4>
+      <ul>
+        <li v-for="item in carrer.intern_jp" v-bind:key="item">{{ item }}</li>
+      </ul>
     </div>
 
     <div v-show="isEnglish">
@@ -20,6 +24,10 @@
       <h4>Work Experience</h4>
       <ul>
         <li v-for="item in carrer.work_en" v-bind:key="item">{{ item }}</li>
+      </ul>
+      <h4>Intern</h4>
+      <ul>
+        <li v-for="item in carrer.intern_en" v-bind:key="item">{{ item }}</li>
       </ul>
     </div>
   </div>
@@ -47,16 +55,23 @@ const carrer = {
     "2017/04 - present. 　Graduate School of Library, Information and Media Studies, University of Tsukuba"
   ],
   "work_jp": [
-    "2015/08 - 2015/09 　チームラボ株式会社 インタラクティブチーム (就業インターン)",
     "2016/12 - 2017/08 　ユニロボット株式会社 Unityエンジニア",
     "2017/09 - 2020/03 　ピクシーダストテクノロジーズ株式会社 リサーチエンジニア",
+    "2020/04 - 2021/01 　株式会社ランドスケイプ 社外CTO",
     "2020/04 - present. 　Telorain株式会社 代表取締役"
   ],
   "work_en": [
-    "2015/08 - 2015/09 　teamLab Inc. Interactive Team (Intern)",
     "2016/12 - 2017/08 　Unirobot Corporation. Unity Enginner",
     "2017/09 - 2020/03 　Pixie Dust Technologies, Inc. Research Enginner",
     "2020/04 - present. 　Telorain Inc. CEO & Founder"
+  ],
+  "intern_jp": [
+    "2015/08 - 2015/09 　チームラボ株式会社 インタラクティブチーム",
+    "2019/12 - 2020/01 　株式会社サイバーエージェント AILab",
+  ],
+  "intern_en": [
+    "2015/08 - 2015/09 　teamLab Inc. Interactive Team",
+    "2019/12 - 2020/01 　CyberAgent, Inc. AILab",
   ]
 }
 
